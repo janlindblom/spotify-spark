c4334442387733b13df0858d4f82ba09e878c943
Jan Lindblom <jan@janlindblom.se>
2012-08-24 12:27:07 +0200

    Adding some files.

---
+++ config/spotify-api.php
@@ -0,0 +1,11 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * @author    Jan Lindblom <jan@janlindblom.se>
+ * @copyright Copyright (c) 2012, Jan Lindblom.
+ * @license   MIT
+ * @version   1.0.0
+ */
+
+$config['spotify_basepath'] = 'http://ws.spotify.com/service';
+$config['spotify_api_version'] = '';
+?>
---
+++ helpers/spotify_helper.php
@@ -0,0 +1,28 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Spotify Web API Helper for CodeIgniter.
+ * 
+ * See https://developer.spotify.com/technologies/web-api/ for details.
+ * 
+ * NOTE! This Helper is in no way endorsed or supported by Spotify.
+ *
+ * @package   helpers
+ * @author    Jan Lindblom <jan@janlindblom.se>
+ * @copyright Copyright (c) 2012, Jan Lindblom.
+ * @license   MIT
+ * @version   1.0.0
+ */
+
+if ( ! function_exists('spotify_lookup')) {
+	function spotify_lookup($str = '') {
+		
+	}
+}
+
+if ( ! function_exists('spotify_search')) {
+	function spotify_search($str = '') {
+		
+	}
+}
+
+?>
